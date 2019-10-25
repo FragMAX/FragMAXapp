7fa7b57ce3c8360b3edc5979a3e16234a6bf4c6c
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2019-10-25 13:22:19 +0200

    be strict with allowed characters in project fields
    Only allow the user to specify project fields that matches
    the respective regex for each field.
    
    This strict validation is due to the fact that we construct
    paths from project's fields, so let be extra paranoid.

--- fragview/forms.py
+++ fragview/forms.py
@@ -1,7 +1,15 @@
+import re
 from os import path
 from django import forms
 from . import projects
 from .models import Project
+
+
+def _is_8_digits(str, subject="shift"):
+    if re.match("^\\d{8}$", str) is None:
+        raise forms.ValidationError(f"invalid {subject} '{str}', should be 8 digits")
+
+    return str
 
 
 class ProjectForm(forms.ModelForm):
@@ -9,8 +17,52 @@ class ProjectForm(forms.ModelForm):
         model = Project
         fields = ("protein", "library", "proposal", "shift", "shift_list")
 
+    #
+    # first each individual field is validated on 'syntactic' level,
+    # i.e. we check that they match specific regexp
+    #
+    # we are overlay strict with all the field's contents, as they are used to build
+    # paths, so we don't allow any funny characters, to avoid potential path hacks
+    #
+
+    def clean_protein(self):
+        protein = self.cleaned_data["protein"]
+
+        if re.match("^\\w+$", protein) is None:
+            raise forms.ValidationError("invalid characters, use numbers and letters")
+
+        return protein
+
+    def clean_proposal(self):
+        return _is_8_digits(self.cleaned_data["proposal"], "proposal")
+
+    def clean_shift(self):
+        return _is_8_digits(self.cleaned_data["shift"])
+
+    def clean_shift_list(self):
+        shift_list = self.cleaned_data["shift_list"].strip()
+
+        if not shift_list:
+            # empty shift list is valid
+            return shift_list
+
+        for shift in shift_list.split(","):
+            _is_8_digits(shift)
+
+        return shift_list
+
+    #
+    # as a second step, perform 'semantic' validation, e.g. check
+    # that all directories for specified proposal, protein, etc
+    # actually exist
+    #
+
     def _validate_shift_list(self, proposal):
-        shift_list = self.cleaned_data["shift_list"]
+        shift_list = self.cleaned_data["shift_list"].strip()
+
+        if not shift_list:
+            # empty shift list, no need validate anything more
+            return {}
 
         for shift in shift_list.split(","):
             if not path.isdir(projects.shift_dir(proposal, shift)):
@@ -21,8 +73,10 @@ class ProjectForm(forms.ModelForm):
         return {}
 
     def clean(self):
-        # TODO: we should not allow wierd characters in all these fields
-        # TODO: before we construct paths, otherwise it a potentional security issue?
+        if self.errors:
+            # there were error(s) during the regexp validation,
+            # we can't do the 'semantic' validation
+            return
 
         errors = dict()
 
