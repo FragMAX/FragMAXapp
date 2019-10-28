ccc7c895a7e2fa7739fcf4db6adcae6c998a7dad
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2019-10-28 16:26:59 +0100

    unittests on 'login_required' middleware

--- fragview/tests/test_middleware.py
+++ fragview/tests/test_middleware.py
@@ -4,28 +4,79 @@ from unittest import mock
 from fragview import middleware
 
 
-class TestNoProjectsRedirect(unittest.TestCase):
+class MiddlewareTestsMixin:
     """
-    test no_projects_redirect() middleware
+    utility class for testing our middleware implementations
     """
     def setUp(self):
         self.get_resp_mock = mock.Mock()
-        self.middleware_callable = \
-            middleware.no_projects_redirect(self.get_resp_mock)
+        self.middleware_callable = self.MIDDLEWARE(self.get_resp_mock)
 
     def _assert_no_redirect(self, result):
         # check that middleware returned 'process request as usual' result
         self.assertEqual(result, self.get_resp_mock.return_value)
 
-    def _request_mock(self, path="/", with_project=False):
+    def _assert_redirect(self, result, redirect_url):
+        self.assertEqual(result.status_code, 302)
+        self.assertEqual(result.url, redirect_url)
+
+    def _request_mock(self, path="/", authenticated=True, with_project=False):
         req = mock.Mock()
-        req.path = path
+        req.path = path  # in our case, path and path_info are the same
+        req.path_info = path
+
         req.session = dict(proposals=["12344321"])
+
+        req.user.is_authenticated = authenticated
 
         if not with_project:
             req.user.get_current_project.return_value = None
 
         return req
+
+
+class TestLoginRequired(MiddlewareTestsMixin, unittest.TestCase):
+    """
+    test login_required() middleware
+    """
+
+    # use 'staticmethod' to create unbound function call to middleware factory function,
+    # otherwise 'MIDDLEWARE' becomes a class method
+    MIDDLEWARE = staticmethod(middleware.login_required)
+
+    def test_logged_in(self):
+        """
+        user is logged in, we should not be redirected
+        """
+        res = self.middleware_callable(self._request_mock())
+        self._assert_no_redirect(res)
+
+    def test_login_required(self):
+        """
+        no user is logged in, we should be redirected to login page
+        """
+        res = self.middleware_callable(self._request_mock(authenticated=False))
+        self._assert_redirect(res, "/accounts/login/?next=/")
+
+    def test_open_url(self):
+        """
+        case when no user is logged in but we are requesting 'open' url,
+        in this case login page, we should not be redirected
+        """
+        request = self._request_mock(path=urls.reverse("login"),
+                                     authenticated=False)
+        res = self.middleware_callable(request)
+        self._assert_no_redirect(res)
+
+
+class TestNoProjectsRedirect(MiddlewareTestsMixin, unittest.TestCase):
+    """
+    test no_projects_redirect() middleware
+    """
+
+    # use 'staticmethod' to create unbound function call to middleware factory function,
+    # otherwise 'MIDDLEWARE' becomes a class method
+    MIDDLEWARE = staticmethod(middleware.no_projects_redirect)
 
     def test_no_redirect(self):
         """
