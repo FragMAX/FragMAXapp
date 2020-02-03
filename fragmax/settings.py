5d228964b539f3313952e9f7e1faf57925536f1a
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2020-02-03 13:37:35 +0100

    use same HPC frontend as MXCube
    This way, if HPC fronend breaks, it probably will be
    fixed faster, as MXCube is used daily.

--- fragmax/settings.py
+++ fragmax/settings.py
@@ -158,7 +158,7 @@ CELERY_RESULT_BACKEND = "redis://localhost"
 #
 
 # the host we use to manage jobs on HPC
-HPC_FRONT_END = "clu0-fe-1"
+HPC_FRONT_END = "clu0-fe-0"
 # user name for issuing jobs,
 # None means use currently logged in user
 HPC_USER = None
