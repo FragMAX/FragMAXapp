67351dc3849f120ed25328a6a88774c99a8fb0d8
Gustavo Lima <gustavo.lima@maxiv.lu.se>
2020-03-02 15:05:27 +0100

    Added average map and event map view for pandda inspections

--- fragview/views/density.py
+++ fragview/views/density.py
@@ -476,6 +476,9 @@ def pandda_consensus(request):
         f"{proj.data_path()}/fragmax/results/pandda/{proj.protein}/{method}/pandda/processed_datasets/" + \
         f"{dataset}{ddtag}_{run}/*BDC*ccp4"
     map2 = glob(glob_pattern)[0].replace("/data/visitors/", "")
+    average_map = map2.split("event")[0]+"ground-state-average-map.native.ccp4"
+    library = proj.library
+    name = map2.split("/")[-2]
 
     summarypath = \
         "biomax/" + proj.proposal + "/" + proj.shift + "/fragmax/results/pandda/" + proj.protein + "/" + method + \
@@ -561,6 +564,9 @@ def pandda_consensus(request):
             "pdb": pdb,
             "2fofc": map2,
             "fofc": map1,
+            "average_map": average_map,
+            "name": name,
+            "library": library,
             "ligand": ligand,
             "center": center,
             "analysed": analysed,
