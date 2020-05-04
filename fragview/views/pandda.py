53122c89eb4f79c538e2031cd88c4b42e18374a0
Gustavo Lima <gustavo.lima@maxiv.lu.se>
2020-05-04 17:21:24 +0100

    Removed index.html
    Clicking Giant link will now redirect to summary page instead of index (broken link)

--- fragview/views/pandda.py
+++ fragview/views/pandda.py
@@ -521,7 +521,7 @@ def giant(request):
 
     if not available_scores:
         # no panda scores files found
-        return render(request, "fragview/index.html")
+        return render(request, "fragview/")
 
     scoreDict = dict()
     for score in available_scores:
