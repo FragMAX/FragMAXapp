df5a9ef5a254fa124309c811c25203a2978deeb4
Gustavo Lima <gustavo.lima@maxiv.lu.se>
2020-05-14 18:41:55 +0100

    Update read_mtz_flags.py

--- static/read_mtz_flags.py
+++ static/read_mtz_flags.py
@@ -3,8 +3,8 @@
 from iotbx.reflection_file_reader import any_reflection_file
 from sys import argv
 
-
-#mtz="/data/visitors/biomax/20180479/20190622/fragmax/results/pandda/AR/bestdp_bestproc/AR-F2XEntry-H01a_1/final_rfill.mtz"
+# mtz="/data/visitors/biomax/20180479/20190622/fragmax/results/pandda/AR/
+# bestdp_bestproc/AR-F2XEntry-H01a_1/final_rfill.mtz"
 
 mtz=argv[1]
 
@@ -13,11 +13,10 @@ hkl_in = any_reflection_file(file_name=mtz)
 miller_arrays = hkl_in.as_miller_arrays()
 
 for ma in miller_arrays[::-1]:
-    flags=ma.info()
+    flags = ma.info()
     if "SIGF" in str(flags):
-            F_SIGF_flags=flags
-r_free_flags=miller_arrays[1].info()
+            F_SIGF_flags = flags
+r_free_flags = miller_arrays[1].info()
 #F_SIGF_flags=miller_arrays[2].info()
-
 
 print r_free_flags,F_SIGF_flags
