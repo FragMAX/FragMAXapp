629f8a7897c867785f8ae58f9e9cb9185784323b
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2020-05-15 09:56:46 +0200

    use python3 style print() to make flake8 happy

--- static/read_mtz_flags.py
+++ static/read_mtz_flags.py
@@ -1,4 +1,5 @@
 #!/mxn/groups/biomax/wmxsoft/ccp4-fragmax/ccp4-7.0/bin/ccp4-python
+from __future__ import print_function
 from iotbx.reflection_file_reader import any_reflection_file
 from sys import argv
 
@@ -18,4 +19,4 @@ for ma in miller_arrays[::-1]:
 r_free_flags = miller_arrays[1].info()
 # F_SIGF_flags=miller_arrays[2].info()
 
-print r_free_flags, F_SIGF_flags #noqa
+print(r_free_flags, F_SIGF_flags)
