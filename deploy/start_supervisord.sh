02c14f359333ea08cf3c97e6ac5597686e0565ae
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2020-05-29 16:40:08 +0200

    make 'mtzdmp' available inside docker

--- deploy/start_supervisord.sh
+++ deploy/start_supervisord.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 #
+# make mtzdmp command available
+#
+export MX_SOFT=/mxn/groups/biomax/wmxsoft
+. /mxn/groups/biomax/wmxsoft/env_setup/ccp4_env.sh
+
+
+#
 # start the supervisord inside 'FragMAX' conda environment
 #
 
