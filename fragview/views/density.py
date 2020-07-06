d27e60af6eac1cb35bebf4d5aba361ccb75290d9
Gustavo Lima <gustavo.alvares.lima@gmail.com>
2020-07-06 12:29:25 +0200

    Fixed results path for docker

--- fragview/views/density.py
+++ fragview/views/density.py
@@ -114,8 +114,8 @@ def show(request):
     ligfit = ligfit.replace("/data/visitors/", "/static/")
     rhofit = rhofit.replace("/data/visitors/", "/static/")
 
-    pdb_path = path.join(path.dirname(path.dirname(rhofit)), "final.pdb")
-    mtz_path = path.join(path.dirname(path.dirname(rhofit)), "final.mtz")
+    pdb_path = path.join(fitres_dir, "final.pdb")
+    mtz_path = path.join(fitres_dir, "final.mtz")
 
     # get xyz for ligands
     blist = blist.replace(" ", "")
