765b3d35e6926f8aa7ccccf2cc1fee38522d0a26
Gustavo Lima <gustavo.alvares.lima@gmail.com>
2020-07-27 08:56:35 +0200

    Improved pandda process/reprocess method

--- worker/data/pandda_prepare_runs.py
+++ worker/data/pandda_prepare_runs.py
@@ -2,6 +2,7 @@ import os
 import sys
 import subprocess
 import shutil
+import csv
 from glob import glob
 from ast import literal_eval
 
@@ -26,11 +27,10 @@ def dataset_exists(dataset_list):
 
 def ground_state_entries(options):
     # process user input and find models for ground state
-    if any([options["useApos"], options["useSelected"]]):
-        apo_datasets = ",".join([x.split("/")[-1] for x in
-                                 glob(path + "/*Apo*")])
+    if any([options["useApos"], options["useSelected"]]) and not options["reprocessZmap"]:
+        apo_datasets = ",".join([x.split("/")[-1] for x in glob(path + "/*Apo*")])
 
-        dtsfilter = options['dtsfilter']
+        dtsfilter = options["dtsfilter"]
         user_defined_gs = dtsfilter.split(":")[-1]
         if all([options["useApos"], options["useSelected"]]):
             total_ground_datasets = user_defined_gs + "," + apo_datasets
@@ -52,6 +52,28 @@ def ground_state_entries(options):
             print("ignoring declared apo datasets for this run")
         else:
             ground_state_parameter = "ground_state_datasets='" + total_ground_datasets + "'"
+
+    elif options["reprocessZmap"]:
+        print("---FragMAXapp Log---")
+        print("FragMAXapp will reprocess your last run using ")
+        print("datasets without peaks in Z-maps as GSM ")
+        logs = glob(path + "/pandda/logs/*.log")
+        if len(logs) > 0:
+            lastlog = sorted(logs)[-1]
+            print(lastlog)
+            with open(lastlog, "r", encoding="utf-8") as logfile:
+                log = logfile.readlines()
+            for line in log:
+                if "Writing PanDDA End-of-Analysis Summary":
+                    with open(path + "/pandda/analyses/pandda_analyse_events.csv", "r",) as readFile:
+                        events = csv.reader(readFile)
+                        events = [x for x in events][1:]
+                    noZmap = [x[0] for x in events]
+                    alldts = [x.split("/")[-1] for x in glob(path + "/" + protein + "*")]
+                    newGroundStates = ",".join(list(set(alldts) - set(noZmap)))
+            ground_state_parameter = "ground_state_datasets='" + newGroundStates + "'"
+        else:
+            ground_state_parameter = ""
     else:
         ground_state_parameter = ""
     return ground_state_parameter
@@ -89,10 +111,20 @@ def pandda_run(method, options):
 
     os.chdir(path)
 
-    command = "pandda.analyse data_dirs='" + path + "/*' " + \
-              ground_state_parameter + " " + options["customPanDDA"] + \
-              " min_build_datasets=" + str(options["min_datasets"]) + " cpus=" + str(options["nproc"])
+    command = (
+        "pandda.analyse data_dirs='"
+        + path
+        + "/*' "
+        + ground_state_parameter
+        + " "
+        + options["customPanDDA"]
+        + " min_build_datasets="
+        + str(options["min_datasets"])
+        + " cpus="
+        + str(options["nproc"])
+    )
 
+    print(command)
     subprocess.call(command, shell=True)
 
     logs = glob(path + "/pandda/logs/*.log")
@@ -105,9 +137,12 @@ def pandda_run(method, options):
             if all([v, options["initpass"]]):
                 if os.path.exists(v[0]):
                     shutil.rmtree(v[0])
-                    if os.path.exists(path + "/fragmax/process/pandda/ignored_datasets/" + options["method"] + "/" + k):
+                    if os.path.exists(
+                        path + "/fragmax/process/pandda/ignored_datasets/" + options["method"] + "/" + k
+                    ):
                         shutil.rmtree(path + "/fragmax/process/pandda/ignored_datasets/" + options["method"] + "/" + k)
                 pandda_run(method, options)
 
 
 pandda_run(method, options)
+
