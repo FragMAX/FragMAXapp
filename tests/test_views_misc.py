34de7618a2987a9c0068b8d7a946edf4bbc8da74
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2020-07-29 10:45:46 +0200

    logview tests: check all template context variables

--- tests/test_views_misc.py
+++ tests/test_views_misc.py
@@ -27,9 +27,10 @@ class TestLogViewer(test.TestCase, ViewTesterMixin):
         # check created template context
         ctx = resp.context
 
-        self.assertEqual(self.log_path, ctx["dataset"])
         self.assertEqual("", ctx["log"])
-        # self.assertEqual("", ctx["downloadPath"])
+        self.assertEqual(self.log_path, ctx["dataset"])
+        self.assertEqual(path.join("/static/biomax", self.PROP1, self.SHIFT, self.LOG), ctx["downloadPath"])
+        self.assertEqual("", ctx["filetype"])
 
     @patch("os.path.exists")
     @patch("builtins.open")
@@ -46,4 +47,5 @@ class TestLogViewer(test.TestCase, ViewTesterMixin):
 
         self.assertEqual(self.log_path, ctx["dataset"])
         self.assertEqual(self.LOG_DATA, ctx["log"])
-        # self.assertEqual("", path.join("/static/biomax", self.PROP1, self.SHIFT, self.LOG), ctx["downloadPath"])
+        self.assertEqual(path.join("/static/biomax", self.PROP1, self.SHIFT, self.LOG), ctx["downloadPath"])
+        self.assertEqual("txt", ctx["filetype"])
