07927bd7e314cc0931ca8cb10304291e480b057e
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2020-08-17 17:10:18 +0200

    on analysis form, use project_datasets()
    project_datasets() function to list available
    datasets for the 'Data Analysis' form.

--- fragview/views/analysis.py
+++ fragview/views/analysis.py
@@ -1,27 +1,23 @@
 from django.shortcuts import render
 from fragview.models import PDB
-from fragview.projects import current_project, project_raw_master_h5_files
+from fragview.projects import current_project
 from fragview.sites import SITE
 from glob import glob
+from fragview.projects import project_datasets
 
 
 def processing_form(request):
     proj = current_project(request)
 
-    datasets = sorted(
-        [
-            x.split("/")[-1].replace("_master.h5", "")
-            for x in project_raw_master_h5_files(proj)
-            if "ref-" not in x
-        ],
-        key=lambda x: ("Apo" in x, x),
-    )
+    datasets = sorted(project_datasets(proj), key=lambda x: ("Apo" in x, x))
+
     methods = [
         x.split("/")[10]
         for x in glob(
             f"{proj.data_path()}/fragmax/results/pandda/{proj.protein}/*/pandda/analyses/*inspect_events*"
         )
     ]
+
     return render(
         request,
         "fragview/data_analysis.html",
---
+++ tests/test_views_analysis.py
@@ -0,0 +1,53 @@
+from unittest.mock import patch
+from django.test import TestCase
+from django.urls import reverse
+from tests.utils import ViewTesterMixin
+from fragview.models import PDB
+from fragview.sites.plugin import Pipeline
+
+PIPELINES = [Pipeline.BUSTER, Pipeline.XIA2_XDS]
+
+
+class MockSitePlugin:
+    PROPOSALS_DIR = "/foo"
+
+    def get_supported_pipelines(self):
+        return PIPELINES
+
+    def get_project_datasets(self, project):
+        return ["Foo-Lib-A0_1", "hCAII-Apo25_1", "Foo-Lib-B0_2"]
+
+
+class TestProcessingForm(TestCase, ViewTesterMixin):
+    def setUp(self):
+        self.setup_client()
+        self.setup_project()
+
+        # add PDB to our project
+        self.pdb = PDB(project=self.proj, filename="moin.pdb")
+        self.pdb.save()
+
+    def test_view(self):
+        """
+        test the 'data analysis' form view
+        """
+        site = MockSitePlugin()
+
+        with patch("fragview.views.analysis.SITE", site):
+            with patch("fragview.projects.SITE", site):
+                resp = self.client.get(reverse("data_analysis"))
+
+        self.assertEquals(resp.status_code, 200)
+
+        # check template used
+        self.assert_contains_template(resp, "fragview/data_analysis.html")
+
+        # check created template context
+        ctx = resp.context
+
+        self.assertEqual(
+            ctx["datasets"], ["Foo-Lib-A0_1", "Foo-Lib-B0_2", "hCAII-Apo25_1"]
+        )
+        self.assertEqual(list(ctx["pdbs"]), [self.pdb])
+        self.assertEqual(ctx["methods"], [])
+        self.assertEqual(ctx["pipelines"], PIPELINES)
