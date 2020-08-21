748a206c5c109a6c0bd9a7dae1e6d795184c3eb6
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2020-08-21 17:33:33 +0200

    tests in 'site' template context processor

--- tests/test_context_processors.py
+++ tests/test_context_processors.py
@@ -9,15 +9,51 @@ PROP1 = "2020101"
 PROP2 = "2020102"
 
 
+def _get_request_mock(is_authenticated, current_project=None, session=None):
+    req = Mock()
+    req.user.is_authenticated = is_authenticated
+
+    if current_project is not None:
+        req.user.get_current_project.return_value = current_project
+
+    if session is not None:
+        req.session = session
+
+    return req
+
+
+class TestSite(unittest.TestCase):
+    """
+    test 'site' template context processor
+    """
+    def test_logged_in(self):
+        """
+        check context generated when user is logged in
+        """
+        ctx = context_processors.site(_get_request_mock(True))
+
+        # check that we got "site_logo" image specified
+        self.assertIn("site_logo", ctx)
+
+        # check that we got a dictionary of disabled features
+        self.assertIsInstance(ctx["disabled"], dict)
+
+    def test_no_user(self):
+        """
+        check context generated when no user is logged in
+        """
+        ctx = context_processors.site(_get_request_mock(False))
+
+        # we should have an 'account style' specified
+        self.assertIn("account_style", ctx)
+
+
 class TestProjectsNoUser(unittest.TestCase):
     """
     test using 'projects' template context processor when no user is logged in
     """
     def test_projects_ctx(self):
-        req_mock = Mock()
-        req_mock.user.is_authenticated = False
-
-        ctx = context_processors.projects(req_mock)
+        ctx = context_processors.projects(_get_request_mock(False))
 
         # we expect an empty context when user is not authenticated
         self.assertEqual(ctx, {})
@@ -43,10 +79,9 @@ class TestProjectsLoggedIn(test.TestCase):
         # mock a request where user have access to the project created above
         # and current project is set to 'proj1' project
         #
-        self.req_mock = Mock()
-        self.req_mock.user.is_authenticated = True
-        self.req_mock.user.get_current_project.return_value = self.proj1
-        self.req_mock.session = dict(proposals=[PROP1, PROP2])
+        self.req_mock = _get_request_mock(True,
+                                          current_project=self.proj1,
+                                          session=dict(proposals=[PROP1, PROP2]))
 
     def test_projects_ctx(self):
         ctx = context_processors.projects(self.req_mock)
@@ -56,7 +91,7 @@ class TestProjectsLoggedIn(test.TestCase):
 
         # check 'user projects' list in the context
         self.assertSetEqual(
-            set([self.proj1, self.proj2]),
+            {self.proj1, self.proj2},
             set(ctx["projects"]))
 
 
