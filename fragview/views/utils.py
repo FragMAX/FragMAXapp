f57bb55699de0f8b2017b89cfa516236c5f4464e
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2020-09-11 10:46:34 +0200

    remove unused code

--- fragview/views/utils.py
+++ fragview/views/utils.py
@@ -1,10 +1,3 @@
-def open_txt(filename):
-    """
-    utility wrapper to open a text file with utf-8 encoding
-    """
-    return open(filename, "r", encoding="utf-8")
-
-
 def scrsplit(a, n):
     k, m = divmod(len(a), n)
     lst = (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
