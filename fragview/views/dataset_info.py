7ee04e2d4dd8c1b6910d7906d7618ccef2406803
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2021-01-27 12:02:54 +0100

    WIP: use correct src image for the second diffraction image
    use correct 'half way' diffraction image for the second
    diffraction jpeg picture
    
    TBD: this patch breaks on MAXIV deployment, figure out site-portable
    way to deal with this.

--- fragview/views/dataset_info.py
+++ fragview/views/dataset_info.py
@@ -103,8 +103,6 @@ def show(request, images, prefix, run):
 
     proj = current_project(request)
 
-    images = str(images / 2)
-
     dataset_dir = path.join(project_process_protein_dir(proj), prefix, f"{prefix}_{run}")
 
     curp = proj.data_path()
@@ -125,8 +123,6 @@ def show(request, images, prefix, run):
     soakTime = "N/A"
 
     snapshots = [spath.replace("/mxn/groups/ispybstorage/", "/static/") for spath in xsdata.snapshots]
-
-    half = int(float(images) / 200)
 
     #
     # Logs for data processing tools
@@ -222,7 +218,7 @@ def show(request, images, prefix, run):
             "soakTime": soakTime,
             "xsdata": xsdata,
             "snapshots": snapshots,
-            "diffraction_half": half,
+            "diffraction_half": int(images / 2),
             "energy": energy,
             "totalExposure": totalExposure,
             "edgeResolution": edgeResolution,
