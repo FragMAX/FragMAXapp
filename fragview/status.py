d4ea8697107f80573cf687b14e4a457eca31b382
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2021-03-29 15:09:34 +0200

    remove 'Update Project Database' button
    Drop the 'update project' feature. Let's hope nobody
    misses it.
    
    The project should not be able to get out-of-sync, if it
    does, we should fix the bugs.

--- fragview/status.py
+++ fragview/status.py
@@ -1,11 +1,3 @@
-from fragview.projects import (
-    project_script,
-    project_scripts_dir,
-    project_syslog_path,
-    project_datasets,
-    parse_dataset_name,
-    UPDATE_STATUS_SCRIPT,
-)
 from fragview.scraper import (
     scrape_processing_outcome,
     scrape_refine_results,
@@ -15,7 +7,6 @@ from fragview.sites.plugin import Pipeline
 from fragview.dsets import get_datasets
 from fragview.dsets import update_dataset_status
 from fragview.results import update_dataset_results
-from jobs.client import JobsSet
 from fragview import dist_lock
 
 AUTOPROC_TOOLS = {
@@ -66,33 +57,3 @@ def set_imported_autoproc_status(project):
             # TODO: allstatus.csv for each dataset/tool combination,
             # TODO: which is, ehhh, somewhat un-optimal
             update_tool_status(project, tool, dset_name)
-
-
-def run_update_status(proj):
-    jobs = JobsSet("update_status")
-    hpc = SITE.get_hpc_runner()
-
-    #
-    # generate the wrapper batch script
-    #
-    batch = hpc.new_batch_file(
-        "init_project_status",
-        project_script(proj, "update_status.sh"),
-        project_syslog_path(proj, "init_project_status_%j.out"),
-        project_syslog_path(proj, "init_project_status_%j.err"),
-    )
-
-    batch.load_python_env()
-    batch.add_command(f"cd {project_scripts_dir(proj)}")
-
-    for dset in project_datasets(proj):
-        # for each dataset, run the update status script
-        dataset, run = parse_dataset_name(dset)
-
-        batch.add_command(
-            f"python3 ./{UPDATE_STATUS_SCRIPT} {proj.data_path()} {dataset} {run}"
-        )
-
-    batch.save()
-    jobs.add_job(batch)
-    jobs.submit()
--- fragview/views/datasets.py
+++ fragview/views/datasets.py
@@ -1,18 +1,11 @@
 from django.shortcuts import render
-
 from fragview.projects import current_project
-from fragview.status import run_update_status
 from fragview.dsets import get_datasets
 from fragview.sites import SITE
 
 
 def show_all(request):
     proj = current_project(request)
-
-    resyncStatus = str(request.GET.get("resyncstButton"))
-
-    if "resyncStatus" in resyncStatus:
-        run_update_status(proj)
 
     return render(
         request,
