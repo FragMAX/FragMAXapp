40e10b3fa1aa0ba4babcf76d194055de45d6c47f
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2021-10-13 13:18:15 +0200

    HZB: support new user name format
    The HZB have switched to using username in the 'fragNNN' format.
    
    Update the 'proposal' validation regexp to accept this new format.

--- fragview/sites/hzb/project.py
+++ fragview/sites/hzb/project.py
@@ -6,12 +6,12 @@ class ProjectLayout(plugin.ProjectLayout):
     ROOT_NAME = "User Proposal"
 
     def root(self):
-        return dict(name=self.ROOT_NAME, placeholder="frag200000")
+        return dict(name=self.ROOT_NAME, placeholder="frag200")
 
     def check_root(self, root):
-        if re.match(r"^frag\d{6}$", root) is None:
+        if re.match(r"^frag\d{3,6}$", root) is None:
             raise ProjectLayout.ValidationError(
-                f"invalid {self.ROOT_NAME} '{root}', should be in the format frag20000"
+                f"invalid {self.ROOT_NAME} '{root}', should be in the format fragNNN"
             )
 
     @staticmethod
