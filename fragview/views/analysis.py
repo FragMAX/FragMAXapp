f55c2a6c6c1fc8b663959d37babbb0f38cde16a1
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2021-11-02 11:51:33 +0100

    data analysis ui: sort datasets by name
    On 'Data Analysis' page, under 'Select dataset', display
    datasets sorted by the 'name'.
    
    Previosly, the datasets were listed in undefined order,
    effectivly in random order from user's perspective.
    
    The alphabetically sorted datasets are easier to work with.

--- fragview/views/analysis.py
+++ fragview/views/analysis.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from fragview.sites import SITE
+from fragview.projects import current_project
 
 
 def processing_form(request):
+    project = current_project(request)
     default_ligand_tool, ligand_tools = SITE.get_supported_ligand_tools()
+
+    datasets = sorted(project.get_datasets(), key=lambda d: d.name)
 
     return render(
         request,
         "fragview/data_analysis.html",
         {
             "pipelines": SITE.get_supported_pipelines(),
+            "datasets": datasets,
             "default_ligand_tool": default_ligand_tool,
             "ligand_tools": ligand_tools,
         },
