ca9b20d4de351338c665c878d1d4768c636a7831
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2021-12-03 10:15:20 +0100

    HZB: update suggested pandda.inspect
    The new host on HZB (hkl9) uses by default bash,
    thus, the csh specific script does not work.

--- fragview/sites/hzb/__init__.py
+++ fragview/sites/hzb/__init__.py
@@ -92,7 +92,7 @@ class SitePlugin(plugin.SitePlugin):
         cd_path = Path("~", pandda_path.relative_to(parent))
 
         return (
-            f"source /soft/pxsoft/64/ccp4/ccp4-6.5.0/ccp4-7.0/bin/ccp4.setup-csh; "
+            f"source /soft/pxsoft/64/ccp4/ccp4-6.5.0/ccp4-7.0/bin/ccp4.setup-sh; "
             f"cd {cd_path}; pandda.inspect"
         )
 
