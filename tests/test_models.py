6f1928a44a8994e9c4e000926b9343ce1a4275b8
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2021-12-22 08:56:02 +0100

    tests: User project status setters
    Test setting a pending project to 'ready' and 'failed'
    states.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,5 +1,37 @@
-from fragview.models import User, UserProject
+from fragview.models import User, UserProject, PendingProject
+from django.test import TestCase
 from tests.utils import ProjectTestCase
+
+
+class TestUserProject(TestCase):
+    ERR_MSG = "dummy error msg"
+
+    def setUp(self):
+        self.project = UserProject.create_new("MID2", "20201122")
+        self.assertTrue(self.project.is_pending())
+
+    def test_set_ready(self):
+        """
+        test setting pending project into 'ready' state
+        """
+        self.project.set_ready()
+        self.assertFalse(self.project.is_pending())
+
+    def test_set_failed(self):
+        """
+        test setting error on a pending project
+        """
+
+        # set error on the project
+        self.project.set_failed(self.ERR_MSG)
+
+        # should still be pending
+        self.assertTrue(self.project.is_pending())
+
+        # check that error message was set in the database
+        pend_proj = PendingProject.objects.get(project=self.project.id)
+        self.assertIsNotNone(pend_proj)
+        self.assertEqual(pend_proj.error_message, self.ERR_MSG)
 
 
 class TestUser(ProjectTestCase):
