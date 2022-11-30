9347184a7f29176c70b37a5fc896b82b2574d780
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2022-11-30 12:46:07 +0100

    HZB: update path to conda environment
    The conda environmnet have moved to new path.
    Update the jobsd wrapper script to use correct path.

--- deploy/start_jobsd.sh
+++ deploy/start_jobsd.sh
@@ -4,6 +4,6 @@
 # start the jobsd daemon inside 'FragMAX' conda environment
 #
 
-. /soft/pxsoft/64/pymol_2.1/etc/profile.d/conda.sh
+. /fraghome/fragadm/miniconda3/etc/profile.d/conda.sh
 conda activate FragMAX
 ./jobsd.py --cpu-limit 192
