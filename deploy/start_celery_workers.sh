580ff4356061d977653eb31df815528a5d6fe6ce
Elmir Jagudin <elmir.jagudin@maxiv.lu.se>
2022-12-19 17:09:14 +0100

    use correct home folder
    Use the home folder for 'fragmax-service' account.

--- deploy/start_celery_workers.sh
+++ deploy/start_celery_workers.sh
@@ -2,5 +2,5 @@
 . /mxn/groups/biomax/wmxsoft/phenix/build/setpaths.sh
 
 # set HOME variable, otherwise we get problems running phenix.elbow
-HOME=/home/biomax-service
+HOME=/home/fragmax-service
 celery -A fragmax worker --uid 91121 --gid 1300 --loglevel=info
